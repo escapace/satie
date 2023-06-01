fa0e4760dfc3aa46a658115ddfd53d213d5d493e
Mark Martirosian <hello@markmartirosian.com>
2023-06-01 14:30:17 +0000

    fix: workflow

--- packages/container/scripts/docker-entrypoint.sh
+++ packages/container/scripts/docker-entrypoint.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
-set -o errexit
-set -o nounset
-
-if [ -z ${UID+x} ]; then
-  echo "UID not set"
-  exit 1
-fi
-
-if [ -z ${GID+x} ]; then
-  echo "UID not set"
-  exit 1
-fi
-
-addgroup -S -g "${GID}" node \
-  && adduser -S -u "${UID}" -G node -s /bin/sh node \
-  && umask "${UMASK}" \
-  && mkdir -p /wd \
-  && chown node:node /wd \
-  && cd /wd \
-  && exec sudo -u node node --no-warnings --experimental-vm-modules --enable-source-maps /web-fonts/lib/esm/cli.mjs "$@"
+exec /usr/local/bin/gosu node:node "$@"
